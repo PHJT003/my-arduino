d20d165af26b324a89f07a45ddf18261865b052f
Valerio Villani <Valerio.Villani.2019@live.rhul.ac.uk>
2021-04-06 18:57:56 +0100

    soft-coded blink script

--- at-001_led-blink/src/main.cpp
+++ at-001_led-blink/src/main.cpp
@@ -1,9 +1,19 @@
 #include <Arduino.h>
 
+// declare variables
+int builtin_led = 13;   // LED pin number
+int duty_cycle[2] = {20, 40};    // ON-OFF time (ms)
+
 void setup() {
   // put your setup code here, to run once:
+pinMode(builtin_led, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  digitalWrite(builtin_led, HIGH);    // turn LED ON
+  delay(duty_cycle[0]);
+  digitalWrite(builtin_led, LOW);     // turn LED OFF
+  delay(duty_cycle[1]);
+
 }
